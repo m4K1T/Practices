f3f00375f9ac4a9ccaa35a24372002975e8fae76
m4K1 <maki.tokumura@askul.com>
2019-04-04 17:49:46 +0900

    002を解いた

--- src/main/java/practices/Practice002.java
+++ src/main/java/practices/Practice002.java
@@ -1,18 +1,39 @@
 package main.java.practices;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * 正の整数値を入力し、それが偶数か判定する関数 isOdd を作成しなさい
  */
 
 public class Practice002 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("正の整数を入力してくれたら偶数か奇数か教えてあげます：");
+        try {
+            int n = scanner.nextInt();
+            if (n > 0) {
+                if(isOdd(n)){
+                    System.out.println("これは偶数みたい");
+                }else{
+                    System.out.println("これは奇数みたい");
+                }
+            } else {
+                System.out.println("正の数を入力してくれないと困る");
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("正の整数値じゃないとダメです");
+        }
 
     }
 
     // 偶数判定
     public static boolean isOdd(int n) {
-        return ;
+        if(n % 2 == 0){
+            return true;
+        }
+        return false;
     }
 
 }
